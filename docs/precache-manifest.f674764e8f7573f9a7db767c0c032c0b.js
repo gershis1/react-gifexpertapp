dd6f19753e6690fa3c470c6260dbbec38f715be2
Gerardo Morales <gershis1@gmail.com>
2020-07-25 20:55:44 -0500

    First commit

---
+++ docs/precache-manifest.f674764e8f7573f9a7db767c0c032c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4747a4dadc27fb6bb1c8227c1f52c9bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2567b1ec623065a0b19d",
+    "url": "/static/css/main.0026b268.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2567b1ec623065a0b19d",
+    "url": "/static/js/main.f398bb2b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
